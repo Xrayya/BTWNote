daefed18120d282d5e1ecddfc3c172b5d99564b6
Xrayya <nur.alkhiyar@gmail.com>
2024-07-10 19:37:54 +0700

    chore: reset frontend

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,32 +1,18 @@
-import pluginQuery from "@tanstack/eslint-plugin-query";
-
-export default [
-  ...pluginQuery.configs["flat/recommended"],
-  {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended-type-checked",
-      "plugin:@typescript-eslint/stylistic-type-checked",
-      "plugin:react-hooks/recommended",
-      "plugin:react/recommended",
-      "plugin:react/jsx-runtime",
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    ignorePatterns: ["dist", ".eslintrc.cjs"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["react-refresh"],
-    rules: {
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-    },
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      project: ["./tsconfig.json", "./tsconfig.node.json"],
-      tsconfigRootDir: __dirname,
-    },
   },
-];
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { routeTree } from "./routeTree.gen";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
-)
+);
