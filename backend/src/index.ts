a01916cb463e2a5f593de4ced58cdbe3c69afb26
Xrayya <nur.alkhiyar@gmail.com>
2024-08-24 23:34:47 +0700

    feat(be): `HTTPException` handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 import userRoute from "./routes/user";
-import { cors } from "hono/cors";
+import { HTTPException } from "hono/http-exception";
 
 const backend = new Hono().basePath("/api");
 
@@ -14,6 +15,14 @@ backend.route("/users", userRoute);
 
 backend.get("/", (c) => {
   return c.json({ message: "You are entering the root of BTWNote API." }, 200);
+});
+
+backend.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return c.json({ message: err.message }, err.status);
+  }
+
+  return c.json({ message: "Internal Server Error" }, 500);
 });
 
 export default backend;
